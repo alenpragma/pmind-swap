656aabf56bd6ea4744d97b7e45129b307e66701c
Md Asaduzaman <asadasad9282@gmail.com>
2024-01-11 10:44:03 +0600

    footer design complete

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -9,29 +9,36 @@ import linkedin from '../assets/socialicon/linkedin_icon.png';
 
 const Footer = () => {
   return (
-    <div>
-      <div>
-        <h2 className='text-primary text-2xl font-medium'>Join Our Community</h2>
-        <div className='flex gap-[13px]'>
-          <img className='w-[50px] h-[50px] bg-[#C7Dfff] rounded-[10px]' src={facebook} alt="" />
-          <img className='w-[50px] h-[50px] bg-[#C7Dfff] rounded-[10px]' src={x} alt="" />
-          <img className='w-[50px] h-[50px] bg-[#C7Dfff] rounded-[10px]' src={reddit} alt="" />
-          <img className='w-[50px] h-[50px] bg-[#C7Dfff] rounded-[10px]' src={telegram} alt="" />
-          <img className='w-[50px] h-[50px] bg-[#C7Dfff] rounded-[10px]' src={instagram} alt="" />
-          <img className='w-[50px] h-[50px] bg-[#C7Dfff] rounded-[10px]' src={linkedin} alt="" />
-        </div>
-        <div>
-          <h2 className='text-primary text-lg font-medium'>About Mindchain</h2>
-          <ul className='text-[#4707070]'>
-            <li>Contact</li>
-            <li>About</li>
-            <li>Blog</li>
-          </ul>
+    <div className='mx-auto w-[1140px]'>
+      <div className='h-[164px] bg-[#E6E6E6] mt-[18px] flex gap-[139px] place-items-center'>
 
+        <div className='flex place-items-center gap-11 ps-10'>
+          <h2 className='text-primary text-2xl font-medium'>Join Our Community</h2>
+          <div className='flex gap-[13px]'>
+            <img className='w-[50px] h-[50px] bg-[#C7EAFF] rounded-[10px] p-2' src={facebook} alt="" />
+            <img className='w-[50px] h-[50px] bg-[#C7EAFF] rounded-[10px] p-2' src={x} alt="" />
+            <img className='w-[50px] h-[50px] bg-[#FFC3AD] rounded-[10px] p-2' src={reddit} alt="" />
+            <img className='w-[50px] h-[50px] bg-[#AFE8FF] rounded-[10px] p-2' src={telegram} alt="" />
+            <img className='w-[50px] h-[50px] bg-gradient-to-r from-[#FDD1FB] via-[#FFD6FC ] to-[#FAC8C9] rounded-[10px] p-2' src={instagram} alt="" />
+            <img className='w-[50px] h-[50px]   rounded-[10px] p-2' src={linkedin} alt="" />
+
+
+          </div>
         </div>
+
+
+        <div className='mt-3 mb-11'>
+          <h2 className='text-primary  text-lg font-medium pb-0.5'>About Mindchain</h2>
+          <ul className='text-[#707070] text-[16xpx] font-medium'>
+            <li className='pb-[5px]'>Contact</li>
+            <li className='pb-[5px]'>About</li>
+            <li className='pb-[5px]'>Blog</li>
+          </ul>
+        </div>
+
       </div>
       <div className='bg-[#A5A5A5] text-[#525252] py-6'>
-        <p>All Right Resarved Mindchain 2024</p>
+        <p className='text-center text-sm'>All Right Resarved Mindchain 2024</p>
       </div>
     </div>
   );
